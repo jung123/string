ddfff184f3513364dd1e4c36d7faff7bf1dcb127
Kim Junghyun <wndgua@gmail.com>
2018-12-23 09:42:27 +0000

    create make file and rename thread test_code

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char *argv[] )
+{
+	/* input file */
+
+	/* pattern */
+
+	/* select module */
+
+	/* compile */
+
+	/* processing */
+
+	/* output */
+
+	return 0;
+}
+
+
+
